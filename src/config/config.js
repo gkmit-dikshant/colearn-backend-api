b46c95d063f759b1ae6a5f368dc29b21f4778f68
gkmit-dikshant <dikshant@gkmit.co>
2025-11-12 00:26:48 +0530

    feat: add environment configuration

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+export default {
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
+  DB_NAME: process.env.DB_NAME,
+  DB_HOST: process.env.DB_HOST,
+  DB_DIALECT: process.env.DB_DIALECT,
+  DB_PORT: process.env.DB_PORT,
+};
