d43f02642c21f169a7879f58b0e5bc4e104a70ac
gkmit-dikshant <dikshant@gkmit.co>
2025-11-13 21:46:53 +0530

    fix: update db config keys to match Sequelize expectations and db config file path

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-  DB_USER: process.env.DB_USER,
-  DB_PASS: process.env.DB_PASS,
-  DB_NAME: process.env.DB_NAME,
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_DIALECT: process.env.DB_DIALECT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
 };
