c79f630ee2b83e9e91a990cbcc075ed9a31e48df
gkmit-dikshant <dikshant@gkmit.co>
2025-11-15 19:34:19 +0530

    feat: add Operation role and create-table-operation migration

---
+++ src/migrations/20251115124603-create-table-operation.js
@@ -0,0 +1,32 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("operations", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: { type: Sequelize.STRING(50), allowNull: false, unique: true },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("operations");
+  },
+};
