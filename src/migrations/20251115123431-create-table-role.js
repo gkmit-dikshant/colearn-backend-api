2a63d276ce8fffefaa19e96e3c01b53511c65e9a
gkmit-dikshant <dikshant@gkmit.co>
2025-11-15 18:14:39 +0530

    feat: add Role model and create-table-role migration

---
+++ src/migrations/20251115123431-create-table-role.js
@@ -0,0 +1,33 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("roles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: { type: Sequelize.STRING(100), allowNull: false, unique: true },
+      description: { type: Sequelize.TEXT, allowNull: false },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("roles");
+  },
+};
