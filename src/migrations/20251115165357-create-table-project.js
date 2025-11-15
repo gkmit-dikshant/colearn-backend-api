34409489e8cba61925c583af39a2b2ae94dbfd0d
gkmit-dikshant <dikshant@gkmit.co>
2025-11-15 22:43:30 +0530

    feat: add Project model and create-table-project migration with associations to Skill and UserSkill models

---
+++ src/migrations/20251115165357-create-table-project.js
@@ -0,0 +1,35 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("projects", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: { type: Sequelize.STRING(200), allowNull: false },
+      description: { type: Sequelize.TEXT, allowNull: false },
+      location_id: { type: Sequelize.INTEGER, allowNull: false },
+      status: { type: Sequelize.ENUM("active", "inactive"), defaultValue: "active" },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("projects");
+  },
+};
---
+++ src/models/Project.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Project extends Model {
+    static associate(models) {
+      this.belongsToMany(models.Skill, {
+        through: models.UserSkill,
+        foreignKey: "project_id",
+        otherKey: "skill_id",
+        as: "skills",
+      });
+
+      this.hasMany(models.UserSkill, {
+        foreignKey: "project_id",
+        as: "project_skills",
+      });
+    }
+  }
+  Project.init(
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      title: { type: DataTypes.STRING(200), allowNull: false },
+      description: { type: DataTypes.TEXT, allowNull: false },
+      location_id: { type: DataTypes.INTEGER, allowNull: false },
+      status: { type: DataTypes.ENUM("active", "inactive"), defaultValue: "active" },
+    },
+    {
+      sequelize,
+      modelName: "Project",
+      tableName: "projects",
+    }
+  );
+  return Project;
+};
